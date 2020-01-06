1c4c0482d8f6e7bf0d1771b1d00cfdfd78034b45
Clark-Griswold <dustindavis1@gmail.com>
2020-01-06 19:26:25 +0000

    initial commit

---
+++ assets/js/board.js
@@ -0,0 +1,21 @@
+class Card {
+  constructor(text) {
+    this.text = text;
+  }
+}
+
+class List {
+  contructor(title) {
+    this.title = title;
+    this.cards = [];
+  }
+
+  addCard(text) {
+    var card = new Card(text);
+    this.cards.push(card);
+  }
+}
+
+var list = new List('My first list');
+list.addCard('My first card');
+console.log(list);
